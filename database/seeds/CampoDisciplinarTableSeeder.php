f25dfde4d60f31b3890e3f185316ca2e924c9f9b
adrianhmg <adrian.hmg@gmail.com>
2017-03-11 11:03:43 -0600

    Seeders Autogenerados Vacios Merge branch 'master' of https://github.com/porfirioangel/SistemaDGETI
    # Conflicts:
    #	.idea/SistemaDGETI.iml

---
+++ database/seeds/CampoDisciplinarTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CampoDisciplinarTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
