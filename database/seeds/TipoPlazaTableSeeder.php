c87e168aeabe8f1f824a9f86a01ba6dba16dd6d6
adrianhmg <adrian.hmg@gmail.com>
2017-03-11 11:50:10 -0600

    Nuevo Migrate Merge branch 'master' of https://github.com/porfirioangel/SistemaDGETI
    # Conflicts:
    #	.idea/SistemaDGETI.iml

--- database/seeds/TipoPlazaTableSeeder.php
+++ database/seeds/TipoPlazaTableSeeder.php
@@ -11,6 +11,8 @@ class TipoPlazaTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('TIPO_PLAZA')->insert(['id'=>'1','plaza' => 'Tiempo Completo (40 horas)']);
+        DB::table('TIPO_PLAZA')->insert(['id'=>'2','plaza' => 'Tres Cuartos de Tiempo (30 horas)']);
+        DB::table('TIPO_PLAZA')->insert(['id'=>'3','plaza' => 'Medio Tiempo (20 horas)']);
     }
 }
