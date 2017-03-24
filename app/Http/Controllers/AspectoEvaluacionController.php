88eb753cbe549986897ba15363950f01f8355792
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-03-24 00:24:42 -0600

    Seed de evidencias

--- app/Http/Controllers/AspectoEvaluacionController.php
+++ app/Http/Controllers/AspectoEvaluacionController.php
@@ -47,9 +47,9 @@ class AspectoEvaluacionController extends Controller
             case 'A02':
                 return $this->showAspectoNormativa();
             case 'A03':
-                break;
+                return $this->showAspectoPlanesProgramas();
             case 'A04':
-                break;
+                return $this->showAspectoPlantaDocente();
             case 'A05':
                 break;
             case 'A06':
@@ -88,6 +88,38 @@ class AspectoEvaluacionController extends Controller
         $page_title = 'SNB: Normativa';
         $box_title = 'Normativa';
         $aspecto_index = 2;
+        return view('snb.aspecto_evaluacion')->with(compact('page_title',
+            'box_title', 'aspecto', 'criterios_existencia',
+            'criterios_suficiencia', 'criterios_pertinencia', 'aspecto_index'));
+    }
+
+    public function showAspectoPlanesProgramas()
+    {
+        $aspecto = AspectoEvaluacion::with(
+            'subaspectos_evaluacion.evidencias')->select(['id', 'aspecto',
+            'descripcion'])->where('id', '=', 'A03')->first();
+        $criterios_existencia = CriterioExistencia::all();
+        $criterios_suficiencia = CriterioSuficiencia::all();
+        $criterios_pertinencia = CriterioPertinencia::all();
+        $page_title = 'SNB: Planes y Programas';
+        $box_title = 'Planes y Programas';
+        $aspecto_index = 3;
+        return view('snb.aspecto_evaluacion')->with(compact('page_title',
+            'box_title', 'aspecto', 'criterios_existencia',
+            'criterios_suficiencia', 'criterios_pertinencia', 'aspecto_index'));
+    }
+
+    public function showAspectoPlantaDocente()
+    {
+        $aspecto = AspectoEvaluacion::with(
+            'subaspectos_evaluacion.evidencias')->select(['id', 'aspecto',
+            'descripcion'])->where('id', '=', 'A04')->first();
+        $criterios_existencia = CriterioExistencia::all();
+        $criterios_suficiencia = CriterioSuficiencia::all();
+        $criterios_pertinencia = CriterioPertinencia::all();
+        $page_title = 'SNB: Planta Docente';
+        $box_title = 'Planta Docente';
+        $aspecto_index = 4;
         return view('snb.aspecto_evaluacion')->with(compact('page_title',
             'box_title', 'aspecto', 'criterios_existencia',
             'criterios_suficiencia', 'criterios_pertinencia', 'aspecto_index'));
