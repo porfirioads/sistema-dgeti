461faaa79f5db111ea5daf38680434c160325ea2
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-03-26 14:47:21 -0600

    Slider con imágenes definitivas

--- resources/views/landing_master.blade.php
+++ resources/views/landing_master.blade.php
@@ -18,6 +18,33 @@
 </head>
 
 <body class="minimal-background-gray">
+
+<!-- Navigation -->
+<a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
+<nav id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
+        <li class="sidebar-brand">
+            <a href="#top" onclick=$("#menu-close").click();>Start Bootstrap</a>
+        </li>
+        <li>
+            <a href="#top" onclick=$("#menu-close").click();>Home</a>
+        </li>
+        <li>
+            <a href="#about" onclick=$("#menu-close").click();>About</a>
+        </li>
+        <li>
+            <a href="#services" onclick=$("#menu-close").click();>Services</a>
+        </li>
+        <li>
+            <a href="#portfolio" onclick=$("#menu-close").click();>Portfolio</a>
+        </li>
+        <li>
+            <a href="#contact" onclick=$("#menu-close").click();>Contact</a>
+        </li>
+    </ul>
+</nav>
+
 <header id="top" class="header">
     <div class="row">
         <div class="text-vertical-center">
@@ -48,5 +75,7 @@
 
 @section('particular_scripts')
 @show
+
+<a id="to-top" href="#top" class="btn btn-dark btn-lg"><i class="fa fa-chevron-up fa-fw fa-1x"></i></a>
 </body>
 </html>
